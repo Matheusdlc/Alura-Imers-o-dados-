b9aa4b24aa844bd3793533d0f1ccf5290f8c029c
Matheusdlc <103945092+Matheusdlc@users.noreply.github.com>
2022-05-25 11:30:06 -0300

    Add files via upload

---
+++ imersao aula 1/ime.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+casas_sp = pd.read_excel(r'C:\Users\matheus.cordeiro\Desktop\Nova pasta (2)\ALURA DATA SCIENCE\imersao aula 1\sp.xlsx')
+#print(casas_sp.columns)
+#
+#Index(['Street', 'City', 'propertycard__detailvalue', 'quartos', 'banheiros','vagas', 'price'],dtype='object')
